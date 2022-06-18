f081ebeb6339dd6ee99d21b3ededa0a5b6babdad
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2022-06-18 23:20:24 +0100

    Add hook useState to get values from async method

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,16 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './mainPage.css';
 import Header from './header';
 
 
 function App()
 {
+  // savethe houses since we only get them once and we dont want to lose them
+  const [allHouses, setAllHouses] = useState([])
 
   //Get all the house but only run once
-  useEffect(() =>{
-    const fetchHouses = async () => {
+  useEffect(() =>
+  {
+    const fetchHouses = async () =>
+    {
       const rsp = await fetch("/houses.json");
       const houses = await rsp.json();
+
+      setAllHouses(houses);
     };
   }, []);
 
