44e5b6be49e05b56ae570897eed12191c96fc80b
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2022-06-18 23:13:46 +0100

    Use hook useEffect to load API only once

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './mainPage.css';
 import Header from './header';
 
 
 function App()
 {
+
+  //Get all the house but only run once
+  useEffect(() =>{
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+    };
+  }, []);
+
+
+
+
   return (
     <div className="container">
       <Header subtitle="providing houses all over the world" />
