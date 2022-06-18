377fc31c6acb64f461507730eb02a0cd7431c843
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2022-06-18 22:19:06 +0100

    Create a page and header component

---
+++ src/MainPage/index.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './mainPage.css';
+import Header from './header';
+
+
+function App()
+{
+  return (
+    <div className="container">
+      <Header subtitle="providing houses all over the world" />
+    </div >
+  );
+}
+
+
+export default App;
