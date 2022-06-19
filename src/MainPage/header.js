ec9ae7341a7d2eaf60b6db5c5d5960955f1402fb
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2022-06-19 12:44:18 +0100

    fix master so it works

--- src/MainPage/header.js
+++ src/MainPage/header.js
@@ -1,12 +1,12 @@
 import logo from "./GloboLogo.png";
 
-const Header = (subtitle) => (
+const Header = (props) => (
     <header className="row">
         <div className="col-md-5">
             <img src={logo} className="logo" alt="logo"></img>
         </div>
         <div className="col-md-7 mt-5 subtitle">
-            {subtitle}
+            {props.subtitle}
 
         </div>
     </header>
